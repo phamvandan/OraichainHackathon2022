589923154879bfee3ddad5a1088de87b2a7c1f1c
Pham Van Dan <phamvandan.cntt2@gmail.com>
2022-06-11 21:35:16 +0700

    Update service.py

--- COVID-Net-master/service.py
+++ COVID-Net-master/service.py
@@ -12,11 +12,18 @@ app = Flask(__name__)
 # route http posts to this method
 @app.route('/process', methods=['POST'])
 def process():
-    file = request.files['image']
-    response = do_predict(file.filename)
+    hash_code = request.form['image']
+    filename = hash_code
+    response = do_predict(filename)
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=200, mimetype="application/json")
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    file.save(file.filename)
+    return file.filename
 
 if __name__ == "__main__":
     # start flask app
