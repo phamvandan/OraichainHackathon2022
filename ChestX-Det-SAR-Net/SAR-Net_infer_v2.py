ce47e499b403068038de5cf697aba256133fc0f8
Pham Van Dan <phamvandan.cntt2@gmail.com>
2022-06-11 21:34:46 +0700

    Update SAR-Net_infer_v2.py

--- ChestX-Det-SAR-Net/SAR-Net_infer_v2.py
+++ ChestX-Det-SAR-Net/SAR-Net_infer_v2.py
@@ -17,23 +17,25 @@ def download():
 # route http posts to this method
 @app.route('/process', methods=['POST'])
 def process():
-    file = request.files['image']
+    hash_code = request.form['image']
+    filename = hash_code
     # # convert string of image data to uint8
-    nparr = np.fromstring(file.read(), np.uint8)
     # # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = cv2.imread(filename, cv2.IMREAD_COLOR)
     # cv2.imwrite(file.filename, img)
     image_shape = img.shape[:2]
     # DO PREDICT
-    response = do_predict(file.filename)
+    response = do_predict(filename)
     img_drawed = visualize(img, response)
-    cv2.imwrite(os.path.join(RESULTS_FOLDER, file.filename), img_drawed)
-    response['image_download_link'] = '/download?filename=' + file.filename
-    response['image_shape'] = list(image_shape)
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
-    return Response(response=response_pickled, status=200, mimetype="application/json")
+    cv2.imwrite(os.path.join(RESULTS_FOLDER, filename), img_drawed)
+    return filename
 
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    file.save(file.filename)
+    return file.filename
+    
 if __name__ == "__main__":
     # start flask app
     app.run(host="0.0.0.0", port=5000)
